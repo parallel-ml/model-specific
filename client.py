[Fix] fix node stats calculation

client.py
@@ -58,7 +58,7 @@ def master():
     """
     init = Initializer.create()
 
-    for _ in range(30):
+    for _ in range(1000):
         # current frame
         ret, frame = 'unknown', np.random.random_sample(init.input_shape) * 255
         for _ in range(init.split):